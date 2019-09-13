1e5bc50e12185ea8b0b78fbfe779e615a0ab1941
Miguel2368 <54821180+Miguel2368@users.noreply.github.com>
2019-09-13 19:00:11 -0400

    High score

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -209,7 +209,7 @@ public class Player {
 					g.setFont(new Font("Apple Symbols",Font.PLAIN,20));
 					g.setColor(Color.green);
 					g.drawString("Score: " + score, 0,20);
-					g.drawString("High Score: " + highScore, 460, 20);
+					g.drawString("High Score: " + highScore, 455, 20);
 
 
 				}
